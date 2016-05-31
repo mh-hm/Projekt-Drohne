0b1c9ea1452f237a8035ba2b3061cda42d936a85
maddin-maddin <martin-13@aon.at>
2016-05-31 18:31:24 +0200

    Motor controller working

--- Regler 2/Regler 2/src/settings_t.c
+++ Regler 2/Regler 2/src/settings_t.c
@@ -12,21 +12,21 @@ settings_t set;
 void settings_init(void)
 {
 	
-	if (flashcdw_quick_user_page_read(void))
-	{
+	//if (flashcdw_quick_user_page_read(void))
+	//{
 		//User page empty -> default values
 		set.motor_esc_timer_period = 3750;
 		set.motor_esc_timer_value_max = 1875;
 		set.motor_esc_timer_value_min = 937;
 		set.motor_esc_timer_value_interval = set.motor_esc_timer_value_max - set.motor_esc_timer_value_min;
-	}	
-	else
-	{
+	//}	
+	//else
+	//{
 		//TODO: Read user page
 		//uint_fast32_t *pageaddr;
 		//pageaddr = AVR32_USER_PAGE_ADDRESS;
 		//uint16_t val = &pageaddr;
-	}
+	//}
 	
 	
 }
